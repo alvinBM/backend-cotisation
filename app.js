cf5ab8efae664dac09621b5f6612623a171256f2
alvinBAUMA <alvintresor@gmail.com>
2020-10-16 13:32:18 +0100

    Architecture du model Abonne

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const abonnes = require("./controllers/abonnes");
 const bodyParser = require("body-parser");
+import mongoose from "mongoose";
 
 const app = express();
+
+/** Connection a la BD mongoDB */
+mongoose.connect('mongodb+srv://finance243:finance243@cluster0.oxsxg.mongodb.net/backend-cotisation?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !', err));
 
 /** Set response header to allow same parameters */
 app.use((req, res, next) => {
@@ -16,6 +24,8 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded());
 
 app.get("/abonnes", abonnes.getAbonnes);
+
+app.get("/abonnes/:telephone", abonnes.getAbonne);
 
 app.post("/abonnes", abonnes.addAbonne);
 
